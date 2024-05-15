8437e5dded98da70588f28fff7fccb0d8f83f3d4
AbdelTID <abdelwizanlah@gmail.com>
2024-05-15 20:11:25 +0100

    up

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ load_dotenv() ##load all the nevironment variables
 from transformers import pipeline
 
 # Create a speech recognition pipeline using a pre-trained model
-# pipe = pipeline("automatic-speech-recognition", model="chrisjay/fonxlsr")
+pipe = pipeline("automatic-speech-recognition", model="chrisjay/fonxlsr")
 
 REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
 REPLICATE_MODEL_ENDPOINTSTABILITY = os.getenv("REPLICATE_MODEL_ENDPOINTSTABILITY")
@@ -166,10 +166,10 @@ if submit:
     with st.chat_message("user"):
         st.audio(uploaded_file.read(), format='audio/wav')
         # print(uploaded_file)
-        # transcription = pipe(uploaded_file.read())
+        transcription = pipe(uploaded_file)
         
-        # print("Transcription:", transcription['text'])
-        # button_pressed=transcription
+        print("Transcription:", transcription['text'])
+        button_pressed=transcription['text']
 
 
 if button_cols[0].button(example_prompts[0]):
